f01e8febc1efdc0f62468df90458a99843ff5898
alex.hill@gmail.com <alex.hill@gmail.com>
2024-10-07 22:25:28 +0100

    fix vignette syntax

--- tests/testthat/test-relative-dates.R
+++ tests/testthat/test-relative-dates.R
@@ -1,7 +1,7 @@
 test_that("Using relative and absolute dates gives the same answer", {
   # these take a while, so don't run on CI
   skip_on_ci()
-  dat_absolute <- data.table::fread(test_path("testdata", "delta_full_relative.rds"))
+  dat_absolute <- data.table::fread(system.file("delta_full.rds", package = "epikinetics"))
   mod_absolute <- biokinetics$new(data = dat_absolute, covariate_formula = ~0 + infection_history)
   delta_absolute <- mod_absolute$fit(parallel_chains = 4,
                    iter_warmup = 50,
@@ -11,7 +11,7 @@ test_that("Using relative and absolute dates gives the same answer", {
   set.seed(1)
   trajectories_absolute <- mod_absolute$simulate_individual_trajectories()
 
-  dat_relative <- data.table::fread(system.file("delta_full_relative.rds", package = "epikinetics"))
+  dat_relative <- data.table::fread(test_path("testdata", "delta_full_relative.rds"))
   mod_relative <- biokinetics$new(data = dat_relative, covariate_formula = ~0 + infection_history)
   delta_relative <- mod_relative$fit(parallel_chains = 4,
                    iter_warmup = 50,
