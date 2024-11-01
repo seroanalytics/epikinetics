d3bffff3395461598b136d442730fc2597a4ad43
alex.hill@gmail.com <alex.hill@gmail.com>
2024-11-01 17:44:38 +0000

    modify c++ function

--- src/convert_log_scale_inverse.cpp
+++ src/convert_log_scale_inverse.cpp
@@ -4,7 +4,8 @@ using namespace cpp11;
 
 [[cpp11::register]]
 cpp11::data_frame convert_log2_scale_inverse_cpp(cpp11::writable::list dt,
-                                                cpp11::strings vars_to_transform) {
+                                                 cpp11::strings vars_to_transform,
+                                                 double lower_limit) {
 
   for (int i = 0; i < vars_to_transform.size(); i++) {
     std::string var = vars_to_transform[i];
@@ -13,7 +14,7 @@ cpp11::data_frame convert_log2_scale_inverse_cpp(cpp11::writable::list dt,
     
     // Apply the inverse transformation
     for (int j = 0; j < col.size(); j++) {
-      col[j] = 2 * pow(2, col[j]);
+      col[j] = lower_limit * pow(2, col[j]);
     }
     
     // Replace the column
